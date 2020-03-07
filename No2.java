34b9c8627fd021778e3357eeec37e9279c500082
reskyayu04 <61896053+reskyayu04@users.noreply.github.com>
2020-03-06 19:52:17 -0800

    Add files via upload

---
+++ No2.java
@@ -0,0 +1,5 @@
+public class No2{
+public static void main (String[] args){
+System.out.println("Belajar Bahasa Pemrograman Java");
+}
+}
---
+++ No3.java
@@ -0,0 +1,5 @@
+public class No3{
+public static void main (String[] args){
+System.out.println("Selesai Belajar Pemrograman Java");
+}
+}
